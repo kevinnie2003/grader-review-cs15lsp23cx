7be6b534ff29f3828591a050d6711e31bc0a3622
Kevin Nie <kevinnie2003@163.com>
2023-05-10 18:16:26 -0700

    Update

--- grade.sh
+++ grade.sh
@@ -5,8 +5,6 @@ rm -rf grading-area
 
 mkdir grading-area
 
-
-
 git clone $1 student-submission
 echo 'Finished cloning'
 
@@ -17,8 +15,9 @@ fi
 
 cd grading-area
 cp -r ../student-submission/* .
+cp -r ../TestListExamples.java .
+cp -r ../lib .
 echo 'Finished copying'
-
 
 set +e
 javac -cp $CPATH *.java
@@ -26,28 +25,9 @@ if [ $? -ne 0 ]; then
     echo 'Failed to compile'
     exit 1
 fi
-set -e
+
 echo 'Finished compiling'
 
-cd ..
-
-java -cp $CPATH org.junit.runner.JUnitCore TestRunner > output.txt
-
-if [ $? -ne 0 ]; then
-    echo 'Failed to run'
-    exit 1
-fi
-
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
 echo 'Finished grading'
-
-# Again output redirection will be useful, and also tools like grep could be helpful here
-
-grep -E 'Tests run: [0-9]+, Failures: [1-9][0-9]*' output.txt
-
-
-
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
-
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+tail -n 3 output.txt
