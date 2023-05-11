f8eb2e3b11780b36284c840ea184e140f918f688
Kevin Nie <kevinnie2003@163.com>
2023-05-10 17:21:03 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,8 +5,32 @@ rm -rf grading-area
 
 mkdir grading-area
 
+
+
 git clone $1 student-submission
 echo 'Finished cloning'
+if [ $? -ne 0 ]; then
+    echo 'Failed to clone'
+    exit 1
+fi
+
+cp -r student-submission/src/* grading-area
+
+
+cd grading-area
+set +e
+javac -cp $CPATH *.java
+if [ $? -ne 0 ]; then
+    echo 'Failed to compile'
+    exit 1
+fi
+set -e
+
+java -cp $CPATH org.junit.runner.JUnitCore TestRunner > test-output.txt
+if [ $? -ne 0 ]; then
+    echo 'Failed to run tests'
+    exit 1
+fi
 
 
 # Draw a picture/take notes on the directory structure that's set up after
